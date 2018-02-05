8d01ffd99b5007121ff8e8fa1c07469a55ca6615
Amanda Albuquerque <amandlei@hotmail.com>
2018-02-05 00:54:51 -0200

    Add cart context in main

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,76 @@
 import React from 'react';
+import ProductAPI from '../api';
 import { Switch, Route } from 'react-router-dom';
 import Product  from './Product';
 import ProductDetails from './ProductDetails';
 import Cart from './Cart';
 import Checkout from './Checkout';
 
-console.log(Route);
-const Main = () => (      
+class Main extends React.Component {
+  constructor () {
+    super()
+
+    this.state = {
+      products: ProductAPI,
+
+      cart: {
+        products: []
+      }
+    }
+
+    //this.details = this.details.bind(this);
+  }
+
+  addProduct (product) {
+    const { cart } = this.state
+    product = {...product}
+
+    this.setState({
+      cart: {
+        products: [...cart.products, product]
+      }
+    })
+
+    this.addProductCart = this.addProductCart.bind(this)
+    //this.removeProductFromCart = this.removeProductFromCart.bind(this
+  }
+  removeProduct (productId) {
+        const { cart } = this.state
+
+        this.setState({
+          cart: {
+            ...cart,
+            products: cart.products.filter(product => product._id !== productId)
+          }
+        })
+    }
+
+  addProductCart (product) {
+      const { cart } = this.state
+      product = {...product}
+
+      this.setState({
+        cart: {
+          products: [...cart.products, product]
+        }
+      })
+    }
+
+  removeProductCart (productId) {
+      const { cart } = this.state
+
+      this.setState({
+        cart: {
+          ...cart,
+          products: cart.products.filter(product => product._id !== productId)
+        }
+      })
+    }
+
+  render () {
+        const { cart } = this.state
+       
+        return (      
         <main>
           <Switch>
             <Route exact path='/' component={Product}/>
@@ -14,6 +78,12 @@ const Main = () => (
             <Route path='/cart' component={Cart}/>
             <Route path='/product/:id' component={ProductDetails}/>
           </Switch>
+          <Cart
+            products={cart.products}
+          />
         </main>
     )
+  }
+}
+
 export default Main
--- src/components/Product.js
+++ src/components/Product.js
@@ -2,14 +2,13 @@ import React from 'react';
 import ProductAPI from '../api';
 import { Link } from 'react-router-dom';
 
-
 const Product = () => (
   <div>
     <ul className='products'>
       {
         ProductAPI.all().map(p => (
           <li key={p.id}>
-            <Link to={`/product/${p.id}`}>
+            <Link product={p} to={`/product/${p.id}`}>
               <div className='item'>
                 <h2 className='title'>{p.name}</h2> 
                 <p className='price'>{p.price}</p>
